668ba46e7d5fe278e7bdb7cd44b647733a4e391d
Christian Mendel <chr9mendel@gmail.com>
2022-01-17 18:52:44 -0300

    adicionado novos comandos

--- bot.js
+++ bot.js
@@ -1,24 +1,44 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
 const config = require('./config.json')
 
-client.on('ready',()=>{
+client.on('ready', () => {
     console.log(`Bot foi iniciado, com ${client.users.size} usuarios, em ${client.channels.size} canais, em ${client.guilds.size} servidores`)
-    client.user.setGame(`Eu estou em ${client.guilds.size} servidores`)
+    client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`)
 })
 
-client.on('guildCreate', guild =>{
+client.on('guildCreate', guild => {
     console.log(`Bot entrou no servidor ${guild.name} (id: ${guild.id}). População ${guild.memberCount} membros`)
     client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`)
 })
 
-client.on('guildDelete',guild=>{
-    console.log(`o bot foi removido do servudor: ${guild.name} (id: ${guild.id})`)
+client.on('guildDelete', guild => {
+    console.log(`o bot foi removido do servidor: ${guild.name} (id: ${guild.id})`)
     client.user.setActivity(`Serving ${client.guilds.size} servidores`)
 })
 
-client.on('menssage', async message =>{
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+    if (!message.content.startsWith(config.prefix)) return;
 
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const comando = args.shift().toLowerCase();
+
+    if(comando === "ping") {
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
+    }
+    if(comando === "oi") {
+        const m = await message.channel.send("Oi?");
+        m.edit(`Ola eu sou o SadBox!`);
+    }
+    
 })
 
 client.login(config.token)
