bf83d8e016e7eec1b1037d10e3dcd6ed4a32489a
Christian Mendel <chr9mendel@gmail.com>
2022-01-18 09:43:32 -0300

    adicionando a função play

--- bot.js
+++ bot.js
@@ -1,57 +1,60 @@
+const Discord = require("discord.js");
+const ytdl = require('ytdl-core');
+const prefix = require('./config.json').prefix;
+const token = require('./config.json').token;
+
+const client = new Discord.Client();
+
 const mongoose = require('mongoose');
-
-const Discord = require('discord.js')
-
-const client = new Discord.Client({
-    intents: [
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES
-    ]
-})
-const config = require('./config.json')
 
 const dbURI = "mongodb+srv://sadBox:grupo16@cluster0.jwdqx.mongodb.net/acervo-de-musicas?retryWrites=true&w=majority";
 
 const respostaDoDB = mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
+
 client.on('ready', () => {
-    console.log(`Bot foi iniciado, com ${client.users.size} usuarios, em ${client.channels.size} canais, em ${client.guilds.size} servidores`)
-    client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`)
+    console.log(`Bot foi iniciado, com sucesso!`)
+    client.user.setActivity(`;help`)
 })
 
-client.on('guildCreate', guild => {
-    console.log(`Bot entrou no servidor ${guild.name} (id: ${guild.id}). População ${guild.memberCount} membros`)
-    client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`)
-})
+client.on('message', async (message) => {
 
-client.on('guildDelete', guild => {
-    console.log(`o bot foi removido do servidor: ${guild.name} (id: ${guild.id})`)
-    client.user.setActivity(`Serving ${client.guilds.size} servidores`)
-})
-
-client.on('message', async message => {
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-    if (!message.content.startsWith(config.prefix)) return;
-
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const comando = args.shift().toLowerCase();
+    const args = message.content
+    const comando = args.toLowerCase();
 
     if (comando === "ping") {
         const m = await message.channel.send("Ping?");
-        m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
+        m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms`);
     }
     if (comando === "oi") {
         const m = await message.channel.send("Oi?");
         m.edit(`Ola eu sou o SadBox!`);
     }
-    // if(comando === "play") {
-    //     const x = await message.member.voice.channel.join()
+    if (comando === "tudo bem?") {
+        await message.channel.send("estou muito bem!");
+    }
+    if (comando === "bot vagabundo") {
+        await message.channel.send("vagabundo é você que não me programou direito");
+    }
+    if (comando === "tchau") {
+        await message.channel.send("até logo!");
+    }
+    // if(comando === ";rasteira") {
+    //     const x = await message.member.voice.disconnect()
     //     console.log(x)
     // }
-    if (comando === "autor") {
-        const m = await message.channel.send("Grupo 16");
+    if (comando === ";help") {
+        await message.channel.send("use o comando ;rasteira");
     }
+    if (comando === ";play") {
+        try{
+            await message.member.voice.channel.join()
+            message.reply('Entrando')
+        }catch{
+            message.reply('Erro, verifique se voce esta connectado em canal de voz')
+        }
+    }
+
 })
 
 client.on('message', message => {
@@ -105,4 +108,4 @@ client.on('message', message => {
 })
 
 
-client.login(config.token)
+client.login(token);
